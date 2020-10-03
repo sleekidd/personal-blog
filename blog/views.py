dc09bd3240e51d6fba4e7e43033dba1314929931
thetobiallen <tobi_newsystem@yahoo.com>
2020-10-03 16:30:08 +0100

    Made changes to post lits views

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date')
+        published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
